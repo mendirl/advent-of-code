f0203610cbeef4c9a9be317156919c4b44d5bd1b
Fabien Couillard <couillard.fabien+dev@gmail.com>
2020-12-12 00:35:32 +0100

    add day 5 in kotlin

---
+++ kotlin/src/main/kotlin/io/mendirl/adventofcode/Day5.kt
@@ -0,0 +1,28 @@
+package io.mendirl.adventofcode
+
+class Day5 : Day() {
+    override fun step1(inputs: List<String>): Int {
+        return calcul(inputs).maxOrNull()!!
+    }
+
+    override fun step2(inputs: List<String>): Int {
+        return calcul(inputs).let { seats ->
+            (1..128 * 8).first {
+                !seats.contains(it) && seats.contains(it - 1) && seats.contains(it + 1)
+            }
+        }
+    }
+
+    private fun calcul(inputs: List<String>): List<Int> {
+        return inputs.map {
+            it
+                .replace("F", "0")
+                .replace("L", "0")
+                .replace("B", "1")
+                .replace("R", "1")
+        }.toInt(2)
+    }
+}
+
+
+
