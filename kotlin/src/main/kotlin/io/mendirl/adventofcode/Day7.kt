d86ee6009e95c31b22b81bee5f5346f891fb3eac
Fabien Couillard <couillard.fabien+dev@gmail.com>
2020-12-13 21:17:51 +0100

    add day 7 in kotlin

---
+++ kotlin/src/main/kotlin/io/mendirl/adventofcode/Day7.kt
@@ -0,0 +1,49 @@
+package io.mendirl.adventofcode
+
+class Day7 : Day() {
+    override fun step1(inputs: List<String>): Int {
+        val myBag = "shiny gold"
+        val outerBags = mutableSetOf<String>()
+        val bagQueue = ArrayDeque<String>()
+        bagQueue.add(myBag)
+
+
+        while (bagQueue.isNotEmpty()) {
+            val innerBag = bagQueue.removeLast()
+            inputs.content().filterValues { bags ->
+                bags.any { it.second == innerBag }
+            }.keys.forEach { bag ->
+                if (outerBags.add(bag))
+                    bagQueue.add(bag)
+            }
+        }
+
+        return outerBags.size;
+
+    }
+
+    override fun step2(inputs: List<String>): Int {
+        val myBag = "shiny gold"
+
+        fun contains(bag: String): Int =
+            inputs.content()[bag]!!.sumBy { (n, color) -> n + (n * contains(color)) }
+
+        return contains(myBag)
+    }
+
+}
+
+fun List<String>.content(): Map<String, List<Pair<Int, String>>> {
+    return this.associate { line ->
+        val (bag, contains) = line.split(" bags contain ")
+        val contentList = contains.split(", ")
+            .mapNotNull { s ->
+                val (n, adj, color) = s.split(' ')
+                n.toIntOrNull()?.let { it to "$adj $color" }
+            }
+        bag to contentList
+    }
+
+}
+
+
